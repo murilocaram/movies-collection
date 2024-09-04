cf7914a6c1e467b9264b7c66f150204a7b58811f
murilo-caram <murilocaram8@gmail.com>
2024-09-04 12:28:44 -0300

    aula front end 01

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+API_URl = 'http://127.0.0.1:5000/api/movies'
+
+@app.route('/', methods=['GET'])
+def index():
+    response = requests.get(API_URl)
+    
+    return render_template('index.html')
+
+if '__name__' == '__main__':
+    app.run(port=5001, debug=True)
