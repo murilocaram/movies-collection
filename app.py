002987b6d99c78c97ef3ab9ca96081869e7d3bdf
murilo-caram <murilocaram8@gmail.com>
2024-09-11 09:11:51 -0300

    njdsgfosun

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
 
-API_URl = 'http://127.0.0.1:5000/api/movies'
+API_URL = 'http://127.0.0.1:5000/api/movies'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    response = requests.get(API_URl)
-    
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        requests.post(
+            API_URL,
+            json={'title':title, 'genre':genre}
+        )
+        return redirect('/')
+    else:
+        response = requests.get(API_URL)    
+        if response.status_code == 200:
+            movies = response.json()
+        else:
+            movies = []
+    return render_template('index.html', movies=movies)
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     app.run(port=5001, debug=True)
